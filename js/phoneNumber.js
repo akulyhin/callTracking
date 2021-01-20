41fca6355f31302eff59b9e6e0ca7c0c34437f70
Artem Kulyhin <akulyhin@gmail.com>
2021-01-20 21:42:52 +0200

    v 1.1

---
+++ js/phoneNumber.js
@@ -0,0 +1,24 @@
+
+// Телефоны по умолчанию
+const defaultPhones = [
+    "+38(098) 745-26-16",
+    "+38(098) 123-45-67",
+    "+38(050) 123-45-67"
+];
+
+// Телефоны для Source=google
+const googlePhones = [
+    "+380Google",
+    "+380Google",
+    "+380Google"
+    ];
+
+//  Телефоны для source=facebook
+const facebookPhones = [
+    "+380Facebook",
+    "+380Facebook",
+    "+380Facebook"
+];
+
+
+export { defaultPhones, googlePhones, facebookPhones };
--- js/script.js
+++ js/script.js
@@ -1,29 +1,28 @@
-const defaultPhones = ["+380987452616", "+380981234567", "+380501234567"];
-const googlePhones = ["+380Google", "+380Google", "+380Google"];
-const facebookPhones = ["+380Facebook", "+380Facebook", "+380Facebook"];
+import { defaultPhones, googlePhones, facebookPhones } from "./phoneNumber.js";
 
 const phoneList = document.querySelector(".phone");
 
 function changePhones(phones) {
   phones.forEach((phone) => {
-    phoneList.insertAdjacentHTML("afterbegin", `<li>${phone}</li>`);
+    const phoneLink = phone.replace(/[() -]/g, '');
+    phoneList.insertAdjacentHTML("beforeend", `<li><a href="tel:${phoneLink}">${phone}</a></li>`);
   });
 }
 
 let href = window.location.href;
 let search = window.location.search;
 
-console.log(href);
-console.log(search);
+// console.log(href);
+// console.log(search);
 
 let utmArr = [];
 
 if (href.indexOf("?") != -1) {
   href = href.slice(0, href.indexOf("?"));
-  search = search.slice(1);
+  search = search.slice(1).toLowerCase();
   utmArr = search.split("&");
 
-  console.log(utmArr);
+  //   console.log(utmArr);
 
   utmArr.map((utm) => {
     localStorage.setItem(utm.split("=")[0], utm.split("=")[1]);
@@ -31,11 +30,12 @@ if (href.indexOf("?") != -1) {
 }
 
 if (localStorage.getItem("utm_source") === "google") {
-  console.log("Google");
+
   changePhones(googlePhones);
 } else if (localStorage.getItem("utm_source") === "facebook") {
-  console.log("Facebook");
+
   changePhones(facebookPhones);
+
 } else {
   changePhones(defaultPhones);
 }
