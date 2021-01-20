1aeab88b2b8ca50a7ac4bbe0023870894feed870
Artem Kulyhin <akulyhin@gmail.com>
2021-01-20 22:10:06 +0200

    v.1.1

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
 import { defaultPhones, googlePhones, facebookPhones } from "./phoneNumber.js";
 
+//Определяем блок по селектору для записи телефонов
 const phoneList = document.querySelector(".phone");
 
+// функция перебора массива телефонов и вставки в наш блок
 function changePhones(phones) {
   phones.forEach((phone) => {
     const phoneLink = phone.replace(/[() -]/g, '');
@@ -9,26 +11,30 @@ function changePhones(phones) {
   });
 }
 
+// Переменные с текущей ссылкой и корнем после слеша
 let href = window.location.href;
 let search = window.location.search;
 
 // console.log(href);
 // console.log(search);
 
+// массив для будущих ютм
 let utmArr = [];
 
+// Определяем ютм, разделям их по знаку "&", записываем их раздельно в массив с уменшенным регистром
 if (href.indexOf("?") != -1) {
-  href = href.slice(0, href.indexOf("?"));
   search = search.slice(1).toLowerCase();
+  console.log(search);
   utmArr = search.split("&");
+  console.log(utmArr);
 
-  //   console.log(utmArr);
-
+// перебираем массив и разбиваем каждый элемент по разделителю "=" и записываем в LocalStorage
   utmArr.map((utm) => {
     localStorage.setItem(utm.split("=")[0], utm.split("=")[1]);
   });
 }
 
+// определяем по ключу, если utm_source равен нашей строке, то отправляем в функцию необходимый массив телефонов
 if (localStorage.getItem("utm_source") === "google") {
 
   changePhones(googlePhones);
